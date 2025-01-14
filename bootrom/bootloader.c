kkrentz/filtering-keystone

Replace ed25519 with p256r1 and SHA-3 with SHA-256

bootrom/bootloader.c
@@ -1,73 +1,41 @@
 #include <stddef.h>
-
-#define ED25519_NO_SEED 1
-#include "sha3/sha3.h"
-/* Adopted from https://github.com/orlp/ed25519
-  provides:
-  - void ed25519_create_keypair(t_pubkey *public_key, t_privkey *private_key, t_seed *seed);
-  - void ed25519_sign(t_signature *signature,
-                      const unsigned uint8_t *message,
-                      size_t message_len,
-                      t_pubkey *public_key,
-                      t_privkey *private_key);
-*/
-
-#include "ed25519/ed25519.h"
-/* adopted from
-  provides:
-  - int sha3_init(sha3_context * md);
-  - int sha3_update(sha3_context * md, const unsigned char *in, size_t inlen);
-  - int sha3_final(sha3_context * md, unsigned char *out);
-  types: sha3_context
-*/
-
+#include <stdint.h>
+#include "coap3/coap_internal.h"
 #include "string.h"
-/*
-  provides memcpy, memset
-*/
-
-
-typedef unsigned char byte;
+#include "uECC.h"
 
 // Sanctum header fields in DRAM
-extern byte sanctum_dev_public_key[32];
-extern byte sanctum_dev_secret_key[64];
+extern uint8_t sanctum_dev_public_key[uECC_BYTES * 2];
+extern uint8_t sanctum_dev_secret_key[uECC_BYTES];
 unsigned int sanctum_sm_size = 0x1ff000;
-extern byte sanctum_sm_hash[64];
-extern byte sanctum_sm_public_key[32];
-extern byte sanctum_sm_secret_key[64];
-extern byte sanctum_sm_signature[64];
+extern uint8_t sanctum_sm_hash[SHA_256_DIGEST_LENGTH];
+extern uint8_t sanctum_sm_public_key[uECC_BYTES * 2];
+extern uint8_t sanctum_sm_secret_key[uECC_BYTES];
+extern uint8_t sanctum_sm_signature[uECC_BYTES * 2];
 #define DRAM_BASE 0x80000000
 
 /* Update this to generate valid entropy for target platform*/
-inline byte random_byte(unsigned int i) {
+int random_bytes(uint8_t *dest, unsigned size) {
 #warning Bootloader does not have entropy source, keys are for TESTING ONLY
-  return 0xac + (0xdd ^ i);
-}
-
-void bootloader() {
-	//*sanctum_sm_size = 0x200;
-  // Reserve stack space for secrets
-  byte scratchpad[128];
-  sha3_ctx_t hash_ctx;
-
-  // TODO: on real device, copy boot image from memory. In simulator, HTIF writes boot image
-  // ... SD card to beginning of memory.
-  // sd_init();
-  // sd_read_from_start(DRAM, 1024);
-
+  static uint8_t counter;
   /* Gathering high quality entropy during boot on embedded devices is
    * a hard problem. Platforms taking security seriously must provide
    * a high quality entropy source available in hardware. Platforms
    * that do not provide such a source must gather their own
    * entropy. See the Keystone documentation for further
    * discussion. For testing purposes, we have no entropy generation.
-  */
-
-  // Create a random seed for keys and nonces from TRNG
-  for (unsigned int i=0; i<32; i++) {
-    scratchpad[i] = random_byte(i);
+   */
+  for (unsigned i = 0; i < size; i++) {
+    dest[i] = 0xac + (0xdd ^ counter++);
   }
+  return 1;
+}
+
+void bootloader() {
+  // TODO: on real device, copy boot image from memory. In simulator, HTIF writes boot image
+  // ... SD card to beginning of memory.
+  // sd_init();
+  // sd_read_from_start(DRAM, 1024);
 
   /* On a real device, the platform must provide a secure root device
      keystore. For testing purposes we hardcode a known private/public
@@ -75,28 +43,28 @@ void bootloader() {
   // TEST Device key
   #include "use_test_keys.h"
   
-  // Derive {SK_D, PK_D} (device keys) from a 32 B random seed
-  //ed25519_create_keypair(sanctum_dev_public_key, sanctum_dev_secret_key, scratchpad);
-
   // Measure SM
-  sha3_init(&hash_ctx, 64);
-  sha3_update(&hash_ctx, (void*)DRAM_BASE, sanctum_sm_size);
-  sha3_final(sanctum_sm_hash, &hash_ctx);
+  sha_256_hash((void*)DRAM_BASE, sanctum_sm_size, sanctum_sm_hash);
 
-  // Combine SK_D and H_SM via a hash
-  // sm_key_seed <-- H(SK_D, H_SM), truncate to 32B
-  sha3_init(&hash_ctx, 64);
-  sha3_update(&hash_ctx, sanctum_dev_secret_key, sizeof(*sanctum_dev_secret_key));
-  sha3_update(&hash_ctx, sanctum_sm_hash, sizeof(*sanctum_sm_hash));
-  sha3_final(scratchpad, &hash_ctx);
-  // Derive {SK_D, PK_D} (device keys) from the first 32 B of the hash (NIST endorses SHA512 truncation as safe)
-  ed25519_create_keypair(sanctum_sm_public_key, sanctum_sm_secret_key, scratchpad);
+  // Generate SM key pair
+  if (!uECC_make_key(sanctum_sm_public_key, sanctum_sm_secret_key, uECC_CURVE())) {
+    while (1);
+  }
 
   // Endorse the SM
-  memcpy(scratchpad, sanctum_sm_hash, 64);
-  memcpy(scratchpad + 64, sanctum_sm_public_key, 32);
-  // Sign (H_SM, PK_SM) with SK_D
-  ed25519_sign(sanctum_sm_signature, scratchpad, 64 + 32, sanctum_dev_public_key, sanctum_dev_secret_key);
+  {
+    sha_256_context_t hash_ctx;
+    uint8_t digest[SHA_256_DIGEST_LENGTH];
+
+    SHA_256.init(&hash_ctx);
+    SHA_256.update(&hash_ctx, sanctum_sm_hash, sizeof(sanctum_sm_hash));
+    SHA_256.update(&hash_ctx, sanctum_sm_public_key, sizeof(sanctum_sm_public_key));
+    SHA_256.finalize(&hash_ctx, digest);
+    // Sign (H_SM, PK_SM) with SK_D
+    if (!uECC_sign(sanctum_dev_secret_key, digest, sizeof(digest), sanctum_sm_signature, uECC_CURVE())) {
+      while (1);
+    }
+  }
 
   // Clean up
   // Erase SK_D

kkrentz/keystone-public

bootrom/bootloader.c
@@ -1,73 +1,41 @@
 #include <stddef.h>
-
-#define ED25519_NO_SEED 1
-#include "sha3/sha3.h"
-/* Adopted from https://github.com/orlp/ed25519
-  provides:
-  - void ed25519_create_keypair(t_pubkey *public_key, t_privkey *private_key, t_seed *seed);
-  - void ed25519_sign(t_signature *signature,
-                      const unsigned uint8_t *message,
-                      size_t message_len,
-                      t_pubkey *public_key,
-                      t_privkey *private_key);
-*/
-
-#include "ed25519/ed25519.h"
-/* adopted from
-  provides:
-  - int sha3_init(sha3_context * md);
-  - int sha3_update(sha3_context * md, const unsigned char *in, size_t inlen);
-  - int sha3_final(sha3_context * md, unsigned char *out);
-  types: sha3_context
-*/
-
+#include <stdint.h>
+#include "coap3/coap_internal.h"
 #include "string.h"
-/*
-  provides memcpy, memset
-*/
-
-
-typedef unsigned char byte;
+#include "uECC.h"
 
 // Sanctum header fields in DRAM
-extern byte sanctum_dev_public_key[32];
-extern byte sanctum_dev_secret_key[64];
+extern uint8_t sanctum_dev_public_key[ECC_CURVE_P_256_SIZE * 2];
+extern uint8_t sanctum_dev_secret_key[ECC_CURVE_P_256_SIZE];
 unsigned int sanctum_sm_size = 0x1ff000;
-extern byte sanctum_sm_hash[64];
-extern byte sanctum_sm_public_key[32];
-extern byte sanctum_sm_secret_key[64];
-extern byte sanctum_sm_signature[64];
+extern uint8_t sanctum_sm_hash[SHA_256_DIGEST_LENGTH];
+extern uint8_t sanctum_sm_public_key[ECC_CURVE_P_256_SIZE * 2];
+extern uint8_t sanctum_sm_secret_key[ECC_CURVE_P_256_SIZE];
+extern uint8_t sanctum_sm_signature[ECC_CURVE_P_256_SIZE * 2];
 #define DRAM_BASE 0x80000000
 
 /* Update this to generate valid entropy for target platform*/
-inline byte random_byte(unsigned int i) {
+int random_bytes(uint8_t *dest, unsigned size) {
 #warning Bootloader does not have entropy source, keys are for TESTING ONLY
-  return 0xac + (0xdd ^ i);
-}
-
-void bootloader() {
-	//*sanctum_sm_size = 0x200;
-  // Reserve stack space for secrets
-  byte scratchpad[128];
-  sha3_ctx_t hash_ctx;
-
-  // TODO: on real device, copy boot image from memory. In simulator, HTIF writes boot image
-  // ... SD card to beginning of memory.
-  // sd_init();
-  // sd_read_from_start(DRAM, 1024);
-
+  static uint8_t counter;
   /* Gathering high quality entropy during boot on embedded devices is
    * a hard problem. Platforms taking security seriously must provide
    * a high quality entropy source available in hardware. Platforms
    * that do not provide such a source must gather their own
    * entropy. See the Keystone documentation for further
    * discussion. For testing purposes, we have no entropy generation.
-  */
-
-  // Create a random seed for keys and nonces from TRNG
-  for (unsigned int i=0; i<32; i++) {
-    scratchpad[i] = random_byte(i);
+   */
+  for (unsigned i = 0; i < size; i++) {
+    dest[i] = 0xac + (0xdd ^ counter++);
   }
+  return 1;
+}
+
+void bootloader() {
+  // TODO: on real device, copy boot image from memory. In simulator, HTIF writes boot image
+  // ... SD card to beginning of memory.
+  // sd_init();
+  // sd_read_from_start(DRAM, 1024);
 
   /* On a real device, the platform must provide a secure root device
      keystore. For testing purposes we hardcode a known private/public
@@ -75,28 +43,28 @@ void bootloader() {
   // TEST Device key
   #include "use_test_keys.h"
   
-  // Derive {SK_D, PK_D} (device keys) from a 32 B random seed
-  //ed25519_create_keypair(sanctum_dev_public_key, sanctum_dev_secret_key, scratchpad);
-
   // Measure SM
-  sha3_init(&hash_ctx, 64);
-  sha3_update(&hash_ctx, (void*)DRAM_BASE, sanctum_sm_size);
-  sha3_final(sanctum_sm_hash, &hash_ctx);
+  sha_256_hash((void*)DRAM_BASE, sanctum_sm_size, sanctum_sm_hash);
 
-  // Combine SK_D and H_SM via a hash
-  // sm_key_seed <-- H(SK_D, H_SM), truncate to 32B
-  sha3_init(&hash_ctx, 64);
-  sha3_update(&hash_ctx, sanctum_dev_secret_key, sizeof(*sanctum_dev_secret_key));
-  sha3_update(&hash_ctx, sanctum_sm_hash, sizeof(*sanctum_sm_hash));
-  sha3_final(scratchpad, &hash_ctx);
-  // Derive {SK_D, PK_D} (device keys) from the first 32 B of the hash (NIST endorses SHA512 truncation as safe)
-  ed25519_create_keypair(sanctum_sm_public_key, sanctum_sm_secret_key, scratchpad);
+  // Generate SM key pair
+  if (!uECC_make_key(sanctum_sm_public_key, sanctum_sm_secret_key, uECC_CURVE())) {
+    while (1);
+  }
 
   // Endorse the SM
-  memcpy(scratchpad, sanctum_sm_hash, 64);
-  memcpy(scratchpad + 64, sanctum_sm_public_key, 32);
-  // Sign (H_SM, PK_SM) with SK_D
-  ed25519_sign(sanctum_sm_signature, scratchpad, 64 + 32, sanctum_dev_public_key, sanctum_dev_secret_key);
+  {
+    sha_256_context_t hash_ctx;
+    uint8_t digest[SHA_256_DIGEST_LENGTH];
+
+    SHA_256.init(&hash_ctx);
+    SHA_256.update(&hash_ctx, sanctum_sm_hash, sizeof(sanctum_sm_hash));
+    SHA_256.update(&hash_ctx, sanctum_sm_public_key, sizeof(sanctum_sm_public_key));
+    SHA_256.finalize(&hash_ctx, digest);
+    // Sign (H_SM, PK_SM) with SK_D
+    if (!uECC_sign(sanctum_dev_secret_key, digest, sizeof(digest), sanctum_sm_signature, uECC_CURVE())) {
+      while (1);
+    }
+  }
 
   // Clean up
   // Erase SK_D